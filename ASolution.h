2f0c72a328cbb182a3848afb53377de241b2444d
s309876 <s309876@55PCH017>
2019-11-23 18:23:04 +0000

    fix Richardson method add analytical solution

---
+++ ASolution.h
@@ -0,0 +1,2 @@
+#pragma once
+void ASolution(double tSur, double tIn, double D, double m, double L, double deltaT, double deltaX);
--- Richardson.h
+++ Richardson.h
@@ -1,5 +1,5 @@
 #pragma once
-double computeNodeR(double iBackwordn, double inBackword, double iForwardn, double D, double deltaT, double deltaX);
+double computeNodeR(double iBackwardn, double in, double iForwardn, double nBackwardi, double D, double deltaT, double deltaX);
 double nextNodeR(double in, double iForwardn, double iBackwardn, double D, double deltaT, double deltaX);
 
 void Richardson(double D, double deltaT, double deltaX);
