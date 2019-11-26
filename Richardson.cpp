aa0a262d1fac42ab0d0848e49709558ef86c049f
s309876 <s309876@55PCH017>
2019-11-26 17:32:01 +0000

    fix

--- Richardson.cpp
+++ Richardson.cpp
@@ -56,16 +56,13 @@ void Richardson(double D, double deltaT, double deltaX) {
 
 			if (j == 8 || j == 18 || j == 28 || j == 38 || j == 48) {
 
-				//myfile << t << "\n";
-				//myfile << nodenext[0] << "\n";
 				myfile << nodenext[i + 1] << "\n";
-				//myfile << nodenext[620] << "\n";
-				//std::cout << nodenext[i + 1] < "\n";
+				
 
 			}
 
 		}
-		//	t += 0.01;
+		
 		for (int i = 0; i < 621; i++) {
 			nodelast[i] = noden[i];
 			noden[i] = nodenext[i];
@@ -73,74 +70,6 @@ void Richardson(double D, double deltaT, double deltaX) {
 
 		}
 	}
-	//n=2
-//	std::cout << "n=2" << "\n";
-//	double node2[621];
-//	node2[0] = 149;
-//	node2[620] = 149;
-//	for (int i = 1; i < 620; i++) {
-//		node2[i] = computeNodeR(node1[i - 1], node1[i], node1[i + 1],node0[i], D, deltaT, deltaX);
-//		//std::cout << node2[i]<<"\n";
-//	}
-//
-//	//n=3
-//	std::cout << "n=3" << "\n";
-//	double node3[621];
-//	node3[0] = 149;
-//	node3[620] = 149;
-//	for (int i = 1; i < 620; i++) {
-//		node3[i] = computeNodeR(node2[i - 1], node1[i], node2[i + 1], node1[i], D, deltaT, deltaX);
-//		//std::cout << node3[i]<<"\n";
-//	}
-//	//n4
-//	std::cout << "n=4" << "\n";
-//	double node4[621];
-//	node4[0] = 149;
-//	node4[620] = 149;
-//	for (int i = 1; i < 620; i++) {
-//		node4[i] = computeNodeR(node3[i - 1], node2[i], node3[i + 1], node2[i], D, deltaT, deltaX);
-//		//std::cout << node4[i] << "\n";
-//	}
-//
-//	//n5
-//	std::cout << "n=5" << "\n";
-//	double node5[621];
-//	node5[0] = 149;
-//	node5[620] = 149;
-//	for (int i = 1; i < 620; i++) {
-//		node5[i] = computeNodeR(node4[i - 1], node3[i], node4[i + 1], node3[i], D, deltaT, deltaX);
-//		//std::cout << node5[i] << "\n";
-//	}
-//	ofstream myfile;
-//	myfile.open("richardsonResultFile.txt");
-//	myfile << "n=1" << "\n";
-//	for (int i = 0; i < 621; i++) {
-//		myfile << node1[i] << " \n";
-//
-//	}
-//	myfile << "n=2" << "\n";
-//	for (int i = 0; i < 621; i++) {
-//		myfile << node2[i] << " \n";
-//
-//	}
-//	myfile << "n=3" << "\n";
-//	for (int i = 0; i < 621; i++) {
-//		myfile << node3[i] << " \n";
-//
-//	}
-//	myfile << "n=4" << "\n";
-//	for (int i = 0; i < 621; i++) {
-//		myfile << node4[i] << " \n";
-//
-//	}
-//	myfile << "n=5" << "\n";
-//	for (int i = 0; i < 621; i++) {
-//		myfile << node5[i] << " \n";
-//
-//	}
-//
-//
-//}
 }
 double nextNodeR(double in, double iForwardn, double iBackwardn, double D, double deltaT, double deltaX) {
 	double Tinplus1;
