4b17599baa5eee72ebe64f74e244799aac40283b
Zikun Wang <Zikun.Wang@cranfield.ac.uk>
2019-12-07 20:05:14 +0000

    解析解

--- ASolution.cpp
+++ ASolution.cpp
@@ -11,7 +11,7 @@ void ASolution(double tSur,double tIn,double D,double m,double L,double deltaT,d
 	double sum = 0;
 	//double node[622];
 	ofstream myfile;
-	myfile.open("ASresultFile.txt");
+	myfile.open("ASresultFile.csv");
 	for (int j = 1; j < 621; j++) {
 		sum = 0;
 		for (int i = 1; i < m; i++) {
@@ -23,11 +23,34 @@ void ASolution(double tSur,double tIn,double D,double m,double L,double deltaT,d
 	   // cout << sum << endl;
 		tNext = tSur + (2 * (tIn - tSur) * sum);
 		node[j] = tNext;
-		myfile << node[j] << "\n";
+		myfile << node[j] << ",";
 		//cout << node[j] << "\n";
 
 		//cout << node[j] << endl;
 
 	}
 	
+	//for (int i = 1; i < 6; i++) {
+	//	double T = 0;
+	//	T = i / 10;
+	//	for (int j = 1; j < 621; j++) {
+	//		sum = 0;
+	//		for (int i = 1; i < m; i++) {
+	//			//sum = 0;
+	//			sum = sum + ((exp((-D) * ((i * pi / L) * (i * pi / L)) * T)) * ((1 - pow(-1, i)) / (i * pi)) * (sin((i * pi * x) / L)));
+	//			//cout << sum << endl;
+	//		}
+	//		x = x + deltaX;
+	//		// cout << sum << endl;
+	//		tNext = tSur + (2 * (tIn - tSur) * sum);
+	//		node[j] = tNext;
+	//		myfile << node[j] << ",";
+	//		//cout << node[j] << "\n";
+
+	//		//cout << node[j] << endl;
+
+	//	}
+	//	myfile << endl;
+	//}
+
 }
--- Main.cpp
+++ Main.cpp
@@ -5,29 +5,64 @@
 #include "Richardson.h"
 #include "ASolution.cpp"
 #include<iostream>
+#include<fstream>
+#include<ostream>
 #include<math.h>
 
 
 int main() {
+	ofstream myfile;
+	myfile.open("SolutionResultFile.csv");
 
+	double i = 0.1;
 	double sum = 0;
 	double laasonenNode[622];//传出数组t=0.01
 	double SolutionNode[622];//solution Result
+	double soulutionNodeTn[622];
 
 
 	//double* laasonenT1 = Lassonen(0.01, 0.05);
 	//DufortFrankel( 0.0093,0.01,0.05); 
 	//Richardson(0.0093, 0.01, 0.05);
 	
-	CrankNicholson(0.01, 0.05);
+	//CrankNicholson(0.01, 0.05);
 
 	//Lassonen(0.01, 0.05,laasonenNode);
 	//cout << node[2];
 	//a = nextNode(149, 38, 38, 0.0093, 0.01, 0.05);
 
 	//std::cout<<a;
-	//ASolution(149,38,93,5000,31,0.01,0.05,SolutionNode);
+	/*ASolution(149,38,93,5000,31,0.1,0.05,SolutionNode);
+	for (int i = 0; i < 622; i++) {
+		myfile << SolutionNode[i] << ",";
+	}
+	myfile << endl;
 	
+	ASolution(149, 38, 93, 5000, 31, 0.2, 0.05, SolutionNode);
+	for (int i = 0; i < 622; i++) {
+		myfile << SolutionNode[i] << ",";
+	}
+	myfile << endl;*/
+	/*for (int i = 1; i <6; i++) {
+		
+		T = i / 10;
+		ASolution(149, 38, 93, 5000, 31, T, 0.05, SolutionNode);
+		for (int j = 0; j < 622; j++) {
+			myfile << SolutionNode[j] << ",";
+		}
+		myfile << endl;
+	}*/
+
+	while (i < 0.6) {
+		ASolution(149, 38, 93, 5000, 31, i, 0.05, SolutionNode);
+		for (int j = 0; j < 622; j++) {
+			myfile << SolutionNode[j] << ",";
+		}
+		myfile << endl;
+		i = i + 0.1;
+	}
+	
+
 	/*for (int i = 1; i < 620; i++) {
 		sum = sum + (laasonenNode[i] - SolutionNode[i]) * (laasonenNode[i] - SolutionNode[i]);	   
 	}
